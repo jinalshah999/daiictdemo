6946143992df459b18baf68b9d8950fafd8dfcee
jinalshah999 <jinalshah999@gmail.com>
2019-09-21 12:45:36 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myfirstapp';
+  no1:number=0;
+  no2:number=0;
+  ans:number=0;
+  name:string="jinal shah";
+  str:string="hello";
+  fun1()
+  {
+    alert("hello");
+  }
+  onAdd(){
+   this.ans=this.no1+this.no2;
+  }
 }
