3644ee8fb76a7630189a471c329edd5cf2971da1
jinalshah999 <jinalshah999@gmail.com>
2019-09-21 14:47:22 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'myfirstapp';
-  no1:number=0;
-  no2:number=0;
-  ans:number=0;
-  name:string="jinal shah";
-  str:string="hello";
-  fun1()
-  {
+  title = "myfirstapp";
+  no1: number = 0;
+  no2: number = 0;
+  ans: number = 0;
+  name: string = "jinal shah";
+  str: string = "hello";
+  Id: number;
+  Title: string;
+  Status: string;
+  arr: string[] = ["jinal", "shah", "abc"];
+
+  tasks: any[] = [
+    { Id: 1, Title: "xyz", Status: "pending" },
+    { Id: 2, Title: "abc", Status: "done" }
+  ];
+  fun1() {
     alert("hello");
   }
-  onAdd(){
-   this.ans=this.no1+this.no2;
+  onAdd() {
+    this.ans = this.no1 + this.no2;
+  }
+  onAddTask() {
+    let obj = { Id: this.Id, Title: this.Title, Status: this.Status };
+    this.tasks.push(obj);
+  }
+  onDeleteTask(item) {
+    let x = this.tasks.indexOf(item);
+    this.tasks.splice(x, 1);
   }
 }
